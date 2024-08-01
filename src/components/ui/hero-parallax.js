d5960d8f427c4064231bc573a09782fae1d4af77
sp-verma <spv7739@gmail.com>
2024-08-01 16:21:00 +0530

    deployement error

--- src/components/ui/hero-parallax.js
+++ src/components/ui/hero-parallax.js
@@ -98,14 +98,15 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold text-white">
-      A Glimpse into Our E-Cell Journey
+        A Glimpse into Our E-Cell&apos;s Journey
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 text-white">
-       Our gallery showcases the essence of the E-Cell's journey at Motihari College of Engineering. Experience the energy of our events, the excitement of our activities, and the achievements of our students through a collection of photos that highlight the spirit of innovation and growth.
+        Our gallery showcases the essence of the E-Cell&apos;s journey at Motihari College of Engineering. Experience the energy of our events, the excitement of our activities, and the achievements of our students through a collection of photos that highlight the spirit of innovation and growth.
       </p>
     </div>
   );
 };
+
 
 export const ProductCard = ({ product, translate }) => {
   return (
